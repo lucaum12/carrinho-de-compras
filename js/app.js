9b329e2dcfd7be68e1fa46689d7f4ea254c5b073
Lucas de Souza <lucassouzarocha1202@gmail.com>
2024-01-24 16:11:06 -0300

    Código refatorado, porém ainda funcional!

--- js/app.js
+++ js/app.js
@@ -1,31 +1,34 @@
-limpar();
+let subTotal = 0;
+document.getElementById("valor-total").textContent = "R$0,00";
+document.getElementById("lista-produtos").textContent = "";
 
 function adicionar() {
-  let produtos = document.getElementById("produto").value;
-  let nomeProduto = produtos.split("-")[0];
-  let valorUnitario = parseFloat(produtos.split("R$")[1]);
+  let produtosDisponiveis = document.getElementById("produto").value;
+  let nomeProduto = produtosDisponiveis.split("-")[0];
+  let valorUnitario = parseFloat(produtosDisponiveis.split("R$")[1]);
   let quantidade = document.getElementById("quantidade").value;
   let carrinho = document.getElementById("lista-produtos");
-  let valorTotal = document.getElementById("valor-total").textContent;
-  let preçoFinal = document.getElementById("valor-total");
-  let preço = parseFloat(valorTotal.split("R$")[1]);
+  let valorTotal = document.getElementById("valor-total");
 
-  if(quantidade <= 0) {
-    alert("Por favor, insira uma quantidade válida!");
+  if(quantidade <= 0 || isNaN(quantidade)) {
+    alert("Insira uma quantidade válida!")
+    document.getElementById("quantidade").value = "";
   } else {
+    subTotal = subTotal + valorUnitario * quantidade;
+
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorUnitario}</span>
-  </section>`;
+    <span class="texto-azul"> ${quantidade}x </span> ${nomeProduto} <span class="texto-azul"> R$${valorUnitario},00 </span>
+    </section>`;
   }
 
-  let valorSubtotal = (valorUnitario * quantidade) + preço;
+  valorTotal.innerHTML = `<span class="texto-azul" id="valor-total"> R$${subTotal},00 </span>`;
 
-  preçoFinal.innerHTML = `<span class="texto-azul" id="valor-total">R$${valorSubtotal.toFixed(2)}</span>`;
   document.getElementById("quantidade").value = "";
 }
 
 function limpar() {
-  document.getElementById("valor-total").innerHTML = "R$0.00";
+  subTotal = 0;
   document.getElementById("quantidade").value = "";
-  document.getElementById("lista-produtos").innerHTML = "";
+  document.getElementById("lista-produtos").textContent = "";
+  document.getElementById("valor-total").textContent = "R$0,00";
 }
